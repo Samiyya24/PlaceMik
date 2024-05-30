510964c0d68f0c7f0774657c5ed85ebc43826626
Samiyya24 <samiyyaazizkhanova904@gmail.com>
2024-05-30 11:04:05 +0500

    added brend section

--- src/components/Brend.vue
+++ src/components/Brend.vue
@@ -1,11 +1,26 @@
 <script setup>
 const props = defineProps({
-  product: Object,
+  marple: Object,
+  required: true,
 });
 </script>
 <template>
-  <div>
-    <img :src="product.img" alt="" />
+  <div class="flex">
+    <div class="bg-white py-8 px-5 shadow-3xl">
+      <div class="flex items-center mb-5 gap-3">
+        <img :src="marple.logo" alt="" />
+        <h1 class="text-2xl">{{ marple.name }}</h1>
+      </div>
+      <div class="flex gap-3">
+        <img
+          class="w-[90px]"
+          v-for="(img, index) in marple.img"
+          :src="img"
+          :key="index"
+          alt=""
+        />
+      </div>
+    </div>
   </div>
 </template>
 
--- src/components/Magazin.vue
+++ src/components/Magazin.vue
@@ -1,13 +1,57 @@
 <script setup>
 import Brend from "./Brend.vue";
-const marple = [
-  { img: ["/images/t-short1.png", "/images/boy1.png", "/images/t-short6.png"] },
+const marples = [
+  {
+    img: ["/images/t-shirt1.png", "/images/boy1.png", "/images/t-shirt6.png"],
+    name: "Marple ",
+    logo: "/images/logo6.png",
+  },
+  {
+    img: ["/images/t-shirt1.png", "/images/boy1.png", "/images/t-shirt6.png"],
+    name: "Huawei",
+    logo: "/images/logo3.png",
+  },
+  {
+    img: ["/images/t-shirt1.png", "/images/boy1.png", "/images/t-shirt6.png"],
+    name: "Berlingo",
+    logo: "/images/logo7.png",
+  },
+  {
+    img: ["/images/t-shirt1.png", "/images/boy1.png", "/images/t-shirt6.png"],
+    name: "U-kwai",
+    logo: "/images/logo2.png",
+  },
+  {
+    img: ["/images/t-shirt1.png", "/images/boy1.png", "/images/t-shirt6.png"],
+    name: "Lassie",
+    logo: "/images/logo4.png",
+  },
+  {
+    img: ["/images/t-shirt1.png", "/images/boy1.png", "/images/t-shirt6.png"],
+    name: "Densel",
+    logo: "/images/logo1.png",
+  },
 ];
 </script>
 <template>
   <div class="container mt-10">
-    <h1 class="text-3xl">Магазины для вас</h1>
-    <div v-for="marple,index in marple"></div>
+    <h1 class="text-3xl mb-7">Магазины для вас</h1>
+    <div class="flex items-center gap-8">
+      <div class="grid grid-cols-3 gap-8">
+        <div v-for="(marple, index) in marples" :key="index">
+          <Brend :marple="marple" />
+        </div>
+      </div>
+      <div class="flex flex-col gap-5 w-[24%]">
+        <img src="/images/image2.png" alt="" />
+        <img src="/images/image1.png" alt="" />
+      </div>
+    </div>
+    <button
+      class="group-hover:block hidden py-2 mt-3 bg-gradient-to-r from-purple-500 to-pink-500 text-white w-full"
+    >
+      Все магазины
+    </button>
   </div>
 </template>
 
